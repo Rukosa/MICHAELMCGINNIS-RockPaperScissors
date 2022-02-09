4f737fd19cb976ac915e6c80d91d59e26277b746
Rukosa <mwmcginnis490@gmail.com>
2022-02-09 14:44:41 -0800

    Initial Commit

---
+++ MICHAELMCGINNIS-RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MICHAELMCGINNIS-RockPaperScissors
+//
+//  Created by Michael Mcginnis on 2/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MICHAELMCGINNIS-RockPaperScissors/MICHAELMCGINNIS_RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_RockPaperScissorsApp.swift
+//  MICHAELMCGINNIS-RockPaperScissors
+//
+//  Created by Michael Mcginnis on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
